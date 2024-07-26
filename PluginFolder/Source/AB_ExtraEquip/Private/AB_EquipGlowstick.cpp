4ad0a2042e76ce3884ee5940735491da33c9fc4e
davidg <david.h.gillen@gmail.com>
2024-07-26 16:59:06 -0600

    Further work on getting glowsticks working.

--- PluginFolder/Source/AB_ExtraEquip/Private/AB_EquipGlowstick.cpp
+++ PluginFolder/Source/AB_ExtraEquip/Private/AB_EquipGlowstick.cpp
@@ -3,3 +3,12 @@
 
 #include "AB_EquipGlowstick.h"
 
+AAB_EquipGlowstick::AAB_EquipGlowstick() {
+	//mCostToUse = 1;
+
+	//FGCharacterPlayer::PickUpBoomBox
+	//FGCharacterPlayer::OnArmsSlotsUnlocked
+	//FGCharacterPlayer::SetupInventory
+	//FGCharacterPlayer::SetupInventory
+	//FGCharacterPlayer::GetTotalPlayerArmEquipmentSlots
+}
--- PluginFolder/Source/AB_ExtraEquip/Public/AB_EquipGlowstick.h
+++ PluginFolder/Source/AB_ExtraEquip/Public/AB_EquipGlowstick.h
@@ -3,15 +3,44 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Equipment/FGEquipment.h"
+#include "Equipment/FGConsumableEquipment.h"
 #include "AB_EquipGlowstick.generated.h"
+
+UENUM(BlueprintType)
+enum class EGlowStickState : uint8
+{
+	EGS_Empty    = 0b00000,
+	EGS_Drawn    = 0b00100,
+	EGS_Cracked  = 0b01001,
+
+	EGS_Drawing  = 0b01101,
+	EGS_Cracking = 0b10001,
+	EGS_Throwing = 0b10111,
+
+	EGM_Lit      = 0b00010,
+	EGM_Anim     = 0b00001
+};
 
 /**
  * 
  */
 UCLASS()
-class AB_EXTRAEQUIP_API AAB_EquipGlowstick : public AFGEquipment
+class AB_EXTRAEQUIP_API AAB_EquipGlowstick : public AFGConsumableEquipment
 {
 	GENERATED_BODY()
 	
+	AAB_EquipGlowstick();
+
+public:
+	UPROPERTY(BlueprintReadWrite, SaveGame, /*Replicated,*/ Category = "GlowStick")
+	EGlowStickState currentState = EGlowStickState::EGS_Empty;
+
+	UPROPERTY(BlueprintReadWrite, /*Replicated,*/ Category = "GlowStick")
+	float hueAngle = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GlowStick")
+	TSubclassOf<AActor> thrownStick;
+
+protected:
+
 };
